c03e3f9ee0bf67927ec49cf3e7f1a79453285bfc
Esteban Fabián Urbano Muñoz <fmthecrow123@gmail.com>
2024-05-02 12:28:56 -0500

    Se sube modelo inscripcion

---
+++ Models/Inscripcion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AcademicoAPI.Models;
+
+public partial class Inscripcion
+{
+    public int IdInscripcion { get; set; }
+
+    public int? Año { get; set; }
+
+    public int? Semestre { get; set; }
+
+    public decimal? Nota { get; set; }
+
+    public int? FkIdMateria { get; set; }
+
+    public int? FkIdEstudiante { get; set; }
+
+    public virtual Estudiante? FkIdEstudianteNavigation { get; set; }
+
+    public virtual Materium? FkIdMateriaNavigation { get; set; }
+}
